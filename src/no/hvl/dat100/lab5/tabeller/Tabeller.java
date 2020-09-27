628c9eb38d51f8202ddbab1db00d27206f0ab7e0
Jørgen <Jrgen@LAPTOP-EOPI4NUG.lan>
2020-09-27 19:30:00 +0200

    Innlevering 2 for gruppe 18

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -5,60 +5,154 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		// Metode som kan skrive ut en tabell meg heltall
+					// for løkke der i=indeks i tabellen, og skriver ut verdi for hver i
+					
+				for (int i=0; i<tabell.length; i++) 
+						System.out.print(tabell[i] + " ");
+						System.out.println();
 
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		//Starte strengen med "[" , avslutte med "]"
+				//Bygger strengen ved å legge til tab[indeks] og et komma ","
+				//For tab[idneks] -1 (siste tall vi legger til), legg til uten komma
+				
+				String streng= ("[");
+				
+				for (int i=0; i<tabell.length; i++) {
+					
+					if (i == tabell.length -1)
+						streng += (tabell[i]);
+					
+					else streng += (tabell[i] + ",");
+				}
+					streng += ("]");
+					
+					return streng;
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
-	}
-
+		//Med while løkke:
+		//int sum=0;
+		//int i=0;
+		//while (i<tabell.length) {
+		//sum += tabell[i];
+		//i++; }
+		//return sum;
+	
+		//Med utvida for-løkke:
+		//int sum=0;
+		//for (int i : tabell) {
+		//sum +=i; }
+		//return sum;
+		
+		//Med vanlig for-løkke
+		int sum= 0;
+		
+		for (int i =0; i<tabell.length;i++) {
+			sum += tabell[i];
+		}
+			return sum;
+			
+		}
+	
+		
+	
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("finnesTall ikke implementert");
+		// Sette funnet til false, while (false) fortsette til neste tabell[indeks]
+		// If tabell[indeks] = tall, funnet = true
+		// return funnet
+		
+		boolean funnet = false;
+		int i=0;	
+		
+		while (!funnet && i<tabell.length) {
+			
+				if (tabell[i] == tall) {
+					funnet= true;
+				}
+				
+				i++;
+		}
+		return funnet;
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
-
-	}
+		//while løkke som for hver indeks sjekker om [i] == tall
+		//endrer verdi til i dersom sann, hvis ikke beholder verdien = -1
+		
+		int verdi = -1;
+		int i =0;
+		
+		while (i<tabell.length && verdi != i) {
+			
+			if (tabell[i] == tall) {
+				verdi = i; }
+			i++; }
+		
+			return verdi;
+			}
+		
 
 	// f)
 	public static int[] reverser(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("reverser ikke implementert");
+		//For løkke, to variable, den ene teller opp den andre end
+		//for indeks 0 i ny tabell settes indeks[tabell.length-1]
+		
+		int []nyTab = new int[tabell.length];
+		
+		for (int i= 0, j= tabell.length-1; i<tabell.length && j>=0; i++, j--) {
+			
+			nyTab[j] = tabell[i];
+		}
+		return nyTab;
 		
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("erSortert ikke implementert");
+		//boolean variabel sort=true
+		//sjekke om [i] er mindre enn [i+1], sort=false dersom den ikke er det
+		//å sjekke for siste indeks vil være out of bounds, så i må være mindre en tablength -1
+		
+		boolean sort = true;
+		
+		for (int i=0; i<tabell.length - 1; i++) {
+			if (tabell[i] > tabell[i +1])
+				sort = false;
+		}
+		return sort;
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
 
-		// TODO
-		throw new UnsupportedOperationException("settSammen ikke implementert");
-	}
+		//Opprette ny tabell med like mange element som de to tabellene tilsammen
+		//En for løkke som setter inn indeks i fra tab1 til indeks i hos tabny
+		//En for løkkge som setter inn indeks i fra tab2 til indeks [i + tab1.length] hos tabny
+		
+		int []tabellNy = new int[tabell1.length + tabell2.length];
+		
+			for (int i= 0; i< tabell1.length; i++) {
+				tabellNy[i] = tabell1[i];
+				}
+				
+				for (int j= 0; j< tabell2.length; j++) {
+					tabellNy[j + tabell1.length] = tabell2[j];
+				}
+			return tabellNy;
+		}
+	
 }
--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -5,46 +5,122 @@ public class Matriser {
 	// a)
 	public static void skrivUt(int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
-	}
+		//To for løkker, ene teller rad andre teller kolonne
+		//printer ut alle verdiene på en rad, og så println
+		
+		for (int rad=0; rad<matrise.length; rad++) {
+			for (int kol=0; kol<matrise[rad].length; kol++) {
+				System.out.print(matrise[rad] [kol] + " ");
+			}
+			System.out.println();
+		}
+		}
+		
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+		//Bruker lignende metode som for tabell, men med to for løkker
 		
+		String streng= ("");
+		
+		for (int rad=0; rad<matrise.length; rad++) {
+			for ( int kol=0; kol<matrise[rad].length; kol++) {
+				streng += (matrise[rad][kol] + " ");
+			}
+			
+			streng += ("\n");
+		}
+			
+			return streng;	
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
+		//Oprette matrise like stor som parameter(matrise)
+		//Gå igjennom hver rad for hver kolonne, multiplisere med (tall) hver gang
+		
+		int [][]mat2 = new int[matrise.length][matrise[0].length];
+		
+		for (int rad=0; rad<mat2.length; rad++) {
+			for ( int kol=0; kol<mat2[rad].length; kol++) {
+				
+				mat2 [rad][kol] = (matrise [rad][kol]) * tall;
+			}
+			}
+		
+	return mat2;
 	
 	}
-
+	
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		//boolean lik=false, to for løkker som sjekker rad og kolonne
+		//sjekke om antall rader er like, hvis ikke lik=false
+		//sjekke om antall kolonner er like hvis ikke lik=false 
+		//Dersom kolonner er like, sjekke om indeks[rad][kol] har lik verdi for a og b, hvis ikke lik=false
+		
+		int rada, kola, radb, kolb;
+		boolean lik = true;
+		
+		rada = a.length;
+		kola = a[0].length;
+		radb = b.length;
+		kolb= b[0].length;
+		
+		if (rada != radb || kola !=kolb)
+			lik = false;
+		
+		else {
+			for (int rad= 0; rad < rada; rad++) {
+				for(int kol= 0; kol < kola; kol++) {
+					if (a[rad][kol] != b[rad][kol]) {
+						lik = false;
+						break;
+					}
+				}
+			}
+		}
+			
+		return lik;
 	}
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("speile ikke implementert");
+		// Ny matrise kopi av parameter, dobbel for løkke for å kopiere element
+		// Ny matrise speil, skal henvise til kopimatrisen med to for løkker
+		// Skal sette verdi for speil[a][b] = kopi[a][b]
+		
+		int [][]kopi = new int[matrise.length][matrise[0].length];
+		
+		for (int rad=0; rad<kopi.length; rad++) {
+			for (int kol=0; kol<kopi[rad].length; kol++) {
+				
+				kopi[rad][kol] = matrise[rad][kol];
+			}
+		}
+		
+		int [][]speil = new int[kopi.length][kopi[0].length];
+		
+		for (int rad=0; rad<speil.length; rad++) {
+			for (int kol=0; kol<speil[rad].length; kol++) {
+				
+				speil[rad][kol] = kopi[kol][rad];
+		
+			}
+		}
+		
+		return speil;
 	
 	}
 
 	// f)
 	public static int[][] multipliser(int[][] a, int[][] b) {
 
-		// TODO
 		throw new UnsupportedOperationException("multipliser ikke implementert");
-	
-	}
+		
+		}
 }
